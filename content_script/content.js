23d1460ed3a39d18f309e1643bd93e26b421379e
ulises-jeremias <ulisescf.24@gmail.com>
2018-04-20 11:27:36 -0300

    Added files for testing

---
+++ content_script/content.js
@@ -0,0 +1,7 @@
+"use strict";
+
+browser.runtime.onMessage.addListener(request => {
+  console.log("Message from the background script:");
+  console.log(request.greeting);
+  return Promise.resolve({response: "Hi from content script"});
+});
