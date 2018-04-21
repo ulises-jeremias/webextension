a849cb3bfedffbb7e75b3235cbb03af839eca9d8
ulises-jeremias <ulisescf.24@gmail.com>
2018-04-21 02:01:13 -0300

    Updated content.js

--- content_script/content.js
+++ content_script/content.js
@@ -12,8 +12,13 @@ builder
 
 var contentHandler = builder.build();
 
+console.log(contentHandler.container);
 console.log(contentHandler.container.id);
 console.log(contentHandler.container.className);
 console.log(contentHandler.close.id);
 console.log(contentHandler.wordsToFetch);
 console.log(contentHandler.wordToFetch());
+
+// Only for testing and debbuging
+document.querySelector("body").appendChild(contentHandler.container);
+contentHandler.container.style.height = "100%";
