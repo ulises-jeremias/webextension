a4712d2b81ebffa2d997f1282ac1e6b2f0671773
ulises-jeremias <ulisescf.24@gmail.com>
2018-04-20 22:47:02 -0300

    Added some methods definitions

--- background/WordFetcher.js
+++ background/WordFetcher.js
@@ -1,7 +1,36 @@
 "use strict"
 
 class WordFetcher {
-  constructor() {
-    
+  constructor({ base = "", path = "/", paramName = "param" }) {
+    this.base = base;
+    this.path = path;
+    this.paramName = paramName;
+  }
+
+  parseHtml(html) {
+    const parser = new DOMParser();
+    return parser.parseFromString(html, "text/html");
+  }
+
+  retrieveElement(elem) {
+    return {};
+  }
+
+  searchWord(word) {
+    return fetch(`${this.base}${this.path}?${this.paramName}=${word}`)
+    .then(response => {
+      if (response.ok) {
+        return response.text();
+      }
+
+      return Promise.reject(new HttpError(`HTTP error: ${response.status}`));
+    })
+    .then(html => this.parseHtml(html))
+    .then(htmlDOM => {
+      return htmlDOM.getElementsByClassName("title");
+    })
+    .catch(error => {
+      Promise.reject(new NetworkError(error.message));
+    });
   }
 }
