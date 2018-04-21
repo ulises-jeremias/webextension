29f9821356c5fa74290c9e0fe6cdb1b71dfd3597
ulises-jeremias <ulisescf.24@gmail.com>
2018-04-21 00:19:43 -0300

    Added class documentation

--- background/WordFetcher.js
+++ background/WordFetcher.js
@@ -1,17 +1,45 @@
+/**
+ * @fileoverview The WordFetcher class definition
+ * @author Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
+ *
+ */
+
 "use strict"
 
 class WordFetcher {
+
+  /**
+   * @constructor
+   *
+   * @param {string}, url base
+   * @param {string}, url path
+   * @param {string}, param name
+   *
+   * @return {WordFetcher}
+   */
   constructor({ base = "", path = "/", paramName = "param" }) {
     this.base = base;
     this.path = path;
     this.paramName = paramName;
   }
 
+  /**
+   * @method Parse html from string
+   *
+   * @param html
+   * @return
+   */
   parseHtml(html) {
     const parser = new DOMParser();
     return parser.parseFromString(html, "text/html");
   }
 
+  /**
+   * This method returns the result of searching for a word on a given page
+   *
+   * @param {string} word
+   * @return {Promise}
+   */
   searchWord(word) {
     return fetch(`${this.base}${this.path}?${this.paramName}=${word}`)
     .then(response => {
