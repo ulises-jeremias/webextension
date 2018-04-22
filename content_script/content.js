fdfe442f2c8d2a5933aabc1292e677be9dc5b4f6
ulises-jeremias <ulisescf.24@gmail.com>
2018-04-22 15:17:43 -0300

    Updated main scripts The idea is that the background defines the tag to be searched so that in the future it is configurable through UI

--- content_script/content.js
+++ content_script/content.js
@@ -1,22 +1,27 @@
 "use strict"
 
-var container = document.createElement('div');
-var close = document.createElement('a');
-var builder = new ContentHandlerBuilder();
+browser.runtime.onMessage.addListener(async message => {
+  const { tagName } = message;
 
-builder
-  .setContainerElement(container)
-  .setCloseElement(close)
-  .setTag('h1')
-  .setSeparator(" ");
+  const container = document.createElement('div');
+  const close = document.createElement('a');
+  const builder = new ContentHandlerBuilder();
 
-var contentHandler = builder.build();
-contentHandler
-  .titlesSearch(contentHandler.wordToFetch())
-  .then(container => {
-    document.querySelector("body").appendChild(container);
-    container.style.height = "100%";
-  })
-  .catch(error => {
-    return Promise.reject(new Error(error.message));
-  });
+  builder
+    .setContainerElement(container)
+    .setCloseElement(close)
+    .setTag(tagName)
+    .setSeparator(" ");
+
+  const contentHandler = builder.build();
+
+  contentHandler
+    .titlesSearch(contentHandler.wordToFetch())
+    .then(container => {
+      document.querySelector("body").appendChild(container);
+      container.style.height = "100%";
+    })
+    .catch(error => {
+      return Promise.reject(new Error(error.message));
+    });
+});
