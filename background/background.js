f925c526ec19223471b580f170801b7a3a1dc5e2
ulises-jeremias <ulisescf.24@gmail.com>
2018-04-22 15:20:34 -0300

    Updated scripts Beautify js

--- background/background.js
+++ background/background.js
@@ -18,11 +18,15 @@ browser.runtime.onMessage.addListener(async message => {
   const {wordToFetch} = message;
   const wordFetcher = new WordFetcher({base, path, paramName});
 
-  return await wordFetcher.searchWord(wordToFetch).then(htmlDOM => {
-    return htmlDOM.getElementsByClassName("gs_rt");
-  }).then(elements => Array.from(elements).map(elem => elem.lastChild)).then(elements => elements.map(elem => {
-    return {title: elem.textContent, href: elem.href};
-  })).catch(error => {
-    Promise.reject(new Error(`NetworkError: ${error.message}`));
-  });
+  return await wordFetcher.searchWord(wordToFetch)
+    .then(htmlDOM => {
+      return htmlDOM.getElementsByClassName("gs_rt");
+    })
+    .then(elements => Array.from(elements).map(elem => elem.lastChild))
+    .then(elements => elements.map(elem => {
+      return {title: elem.textContent, href: elem.href};
+    }))
+    .catch(error => {
+      Promise.reject(new Error(`NetworkError: ${error.message}`));
+    });
 });
