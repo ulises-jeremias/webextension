84cd2931bd552f26a8882db21525e1800bbf11d6
Ulises Jeremias Cornejo Fandos <ulisescf.24@gmail.com>
2018-04-23 08:25:44 -0300

    Update ContentHandlerBuilder.js

--- content_script/ContentHandlerBuilder.js
+++ content_script/ContentHandlerBuilder.js
@@ -13,19 +13,12 @@
  */
 class ContentHandlerBuilder {
   constructor() {
-    this.close = document.createElement('span');
-    this.container = document.getElementsByTagName('div')[0];
+    this.container = 'div';
     this.separator = " ";
     this.tagName = 'h1';
   }
 
-  setCloseElement(htmlElement) {
-    this.close = htmlElement;
-
-    return this;
-  }
-
-  setContainerElement(container) {
+  setContainer(container) {
     this.container = container;
 
     return this;
@@ -52,22 +45,11 @@ class ContentHandlerBuilder {
 
   build() {
     const elements = document.getElementsByTagName(this.tagName);
-    const container = this.container;
-    const close = this.close;
+    const container = document.createElement(this.container);
 
     container.setAttribute("id", "popup_search");
     container.setAttribute("class", "overlay");
 
-    close.setAttribute("id", "popup_close");
-    close.setAttribute("class", "closebtn");
-    close.addEventListener("click", function() {
-      document.getElementById("popup_search").style.height = "0%";
-    });
-
-    close.innerHTML = "&times;";
-
-    container.appendChild(close);
-
-    return new ContentHandler(elements, container, close, this.separator);
+    return new ContentHandler(elements, container, this.separator);
   }
 }
